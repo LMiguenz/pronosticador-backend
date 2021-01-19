056f7723d8f79227623299e8c34d93acc7059881
Leandro Miguenz <leandro.v.059@gmail.com>
2021-01-18 21:53:37 -0300

    Mongoose y conexion con mongodb

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -26,7 +27,16 @@ app.put('/usuario/:id', function (req, res) {
         id
     })
 })
- 
+
+
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(res => console.log('Base de datos conectada'))
+    .catch(error => console.log('Error al conectar con la BD: ', error))
+    
+
+
+
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto', process.env.PORT)
 })
