894a6154e3a80592257fa624e8fdd12775eeece3
Leandro Miguenz <leandro.v.059@gmail.com>
2021-01-21 20:49:30 -0300

    Conexion a mongo atlas

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,14 @@
 //Puerto del servidor
 process.env.PORT = process.env.PORT || 3000
+
+//Definición del entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//Base de datos
+let urlDB
+if(process.env.NODE_ENV === 'dev')
+    urlDB = 'mongodb://localhost:27017/pronosticador'
+else
+    urlDB = 'mongodb+srv://Granjero:kszpgJv1iqqZsPHY@cluster0.mvvo0.mongodb.net/test'
+
+process.env.DB_URL = urlDB
