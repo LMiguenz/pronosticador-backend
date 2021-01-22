48db420912aa0939b29012462bee91402704708a
Leandro Miguenz <leandro.v.059@gmail.com>
2021-01-21 21:36:37 -0300

    la uri de mongo pasa a heroku por seguridad

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,6 @@ let urlDB
 if(process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/pronosticador'
 else
-    urlDB = 'mongodb+srv://Granjero:kszpgJv1iqqZsPHY@cluster0.mvvo0.mongodb.net/Pronosticador'
+    urlDB = process.env.MONGO_URI
 
 process.env.DB_URL = urlDB
