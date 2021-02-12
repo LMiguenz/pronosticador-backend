89fd54d2365ca77433fda1e3835927e025c0fe99
Leandro Miguenz <leandro.miguenz@fluxit.com.ar>
2021-02-12 17:50:20 -0300

    correccion: no se estaban aplicando las validaciones de mongoose al actualizar los datos de un usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -64,7 +64,7 @@ app.put('/usuario/:id', verifyToken, function (req, res) {
     let id = req.params.id
     let body = _.pick(req.body, ['name', 'email', 'role', 'isActive'])
 
-    User.findByIdAndUpdate(id, body, {new: true, runValidators: true}, (err, dbUser) => {
+    User.findByIdAndUpdate(id, body, {new: true, runValidators: true, context: 'query'}, (err, dbUser) => {
         if(err){
             return res.status(400).json({
                 err
