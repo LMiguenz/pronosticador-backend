2be43ece9458e8de2f37b189f36ad93ff8647eb7
Leandro Miguenz <leandro.v.059@gmail.com>
2021-03-28 21:49:39 -0300

    emprolijo endpoints de user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,43 +1,57 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const _ = require('underscore')
-
 const User = require('../models/user')
-
 const {verifyToken, verifyAdminRole} = require('../middleware/auth')
 
 const router = express()
 
 
 router.post('/usuario', function (req, res) {
-    bcrypt.hash(req.body.password, 10)
-        .catch( err => console.log("Password encryption error: ", err))
-        .then( hash => saveUserToDB(req.body, hash, res))
+    let user = new User({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        role: req.body.role
+    })
+
+    createUser(user)
+        .then( dbUser => res.status(200).json({dbUser}) )
+        .catch( error => res.status(error.status).send(error.message))
 })
 
-function saveUserToDB(requestBody, encryptedPassword, res){
-    let user = new User({
-        email: requestBody.email,
-        password: encryptedPassword,
-        name: requestBody.name,
-        role: requestBody.role
-    })
+async function createUser(user){
+    try{
+        const encryptedPassword = await getEncryptedPassword(user.password)
+        return await saveUserToDB(user, encryptedPassword)
+    }catch(error){
+        throw error
+    }
+}
 
-    user.save( (err, dbUser) => {
-        if(err){
-            return res.status(400).json({
-                err
-            })
-        }
+async function getEncryptedPassword(password){
+    try{
+        return await bcrypt.hash(password, 10)
+    }catch(encryptionError){
+        encryptionError.status = 500
+        throw encryptionError
+    }
+}
 
-        res.status(200).json({
-            user: dbUser
-        })
-    })
+async function saveUserToDB(user, encryptedPassword){
+    let userWithEncryptedPassword = user
+    userWithEncryptedPassword.password = encryptedPassword
+
+    try{
+        return await userWithEncryptedPassword.save()
+    }catch(error){
+        let returnError = new Error(error.message)
+        returnError.status = 500
+        throw returnError
+    }
 }
 
 router.get('/usuarios', [verifyToken, verifyAdminRole], function (req, res) {
-
     const offset = Number(req.query.offset || 0)
     const limit = Number(req.query.limit || 0)
 
@@ -63,26 +77,18 @@ router.put('/usuario', verifyToken, function (req, res) {
     let id = req.user.uid
     let body = _.pick(req.body, ['name', 'email', 'role', 'isActive'])
 
-    User.findByIdAndUpdate(id, body, {new: true, runValidators: true, context: 'query'}, 
-        (err, dbUser) => {
-            if(err)
-                return res.status(400).json({ err })
-            else
-                res.status(200).json({ user: dbUser })
-    })
+    User.findByIdAndUpdate(id, body, {new: true, runValidators: true, context: 'query'})
+        .then( dbUser => res.status(200).json({ user: dbUser }))
+        .catch( error => res.status(400).json({ error }))
 })
 
   
 router.delete('/usuario/:id', [verifyToken, verifyAdminRole], function (req, res) {
     let id = req.params.id
 
-    User.findByIdAndUpdate(id, { isActive: false }, {new: true, runValidators: true}, 
-        (err, dbUser) => {
-            if(err)
-                return res.status(400).json({ err })
-            else
-                res.status(200).json({ user: dbUser })
-        })
+    User.findByIdAndUpdate(id, { isActive: false }, {new: true, runValidators: true})
+        .then( dbUser => res.status(200).json({ user: dbUser }))
+        .catch( error => res.status(400).json({ error }))
 })
 
 
